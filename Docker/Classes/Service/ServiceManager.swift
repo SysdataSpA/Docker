882df75ef16a5b23c8d556c4bab69015c7d376d5
Paolo Ardia <paolo.ardia@sysdata.it>
2018-06-14 15:41:31 +0200

    ServiceManager definition

---
+++ Docker/Classes/Service/ServiceManager.swift
@@ -0,0 +1,15 @@
+//
+//  ServiceManager.swift
+//  Pods-Docker
+//
+//  Created by Paolo Ardia on 14/06/18.
+//
+
+import UIKit
+import Alamofire
+
+class ServiceManager: NSObject {
+    let shared = ServiceManager()
+    
+    
+}
